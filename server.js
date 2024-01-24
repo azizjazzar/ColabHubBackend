9be8a8a940cfff2ddf09cc5861bdc6be33a99615
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-01-24 20:54:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
-const hostname = 'localhost';
-const port = 9090;
+const path = require("path"); // Ajoutez cette ligne
 const app = express(); 
 const stripe = require("stripe")('sk_test_51OErmACis87pjNWpHjxy4jOfBeV5X2cD3bB2op5qNVdo8OY7pqpqJh235cFlSwbjNxfjsz6FMZAD1EVCWJs2kyDq00LYDaUrax');
 connectDB();
 app.use(express.json());
-app.set('views','./views')
-app.set('view engine','ejs')
+app.set('views', './public');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res) => {
-  res.render("home")
+  res.render("home");
 });
 
 app.use("/api/auth", require("./routes/auth"));
 
-
-
 app.listen(3000, '0.0.0.0', function() {
-  console.log('Listening to port:  ' + 3000);
-})
+  console.log('Listening to port: ' + 3000);
+});
