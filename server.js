69eed139e188635e50f340a30b8966f205749bb2
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-08 21:53:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
-const path = require("path"); // Ajoutez cette ligne
-const app = express(); 
+const path = require("path");
+const cors = require("cors"); // Ajoutez cette ligne
+const app = express();
 const stripe = require("stripe")('sk_test_51OErmACis87pjNWpHjxy4jOfBeV5X2cD3bB2op5qNVdo8OY7pqpqJh235cFlSwbjNxfjsz6FMZAD1EVCWJs2kyDq00LYDaUrax');
+
 connectDB();
+
 app.use(express.json());
 app.set('views', './public');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Utilisation de CORS middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.render("home");
