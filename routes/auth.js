faf703db472d5440e2841ef07c188d4ed3aa0bec
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-21 05:02:59 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {
-  register, users, getByEmail, remove, update, sendmail, login, logout, getById, refreshToken,updateI,usersI,getByEmailI,registerI,comparePasswords
+  register, users, getByEmail, remove, update, sendmail, login, logout, getById, refreshToken, updateI, usersI, getByEmailI, registerI, comparePasswords, updatePicture // Ajout de la fonction updatePicture
 } = require("../controllers/auth");
 const { verifyTokenMiddleware } = require("../middleware/auth");
 const { verify } = require("crypto");
@@ -10,6 +10,7 @@ router.route("/register").post(register);
 router.route("/update/:email").put(update);
 
 // Assurez-vous que verifyTokenMiddleware est une fonction middleware
+router.route("/updatePicture/:email").put(updatePicture);
 router.route("/users").get( users);
 router.route("/user/:email").get(getByEmail);
 router.route("/email/:email/:code").get(sendmail);
