7a202c85e08ca944fa7f7fb77f35a5314da5a978
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-22 01:58:27 +0100

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,22 +2,14 @@ const multer = require("multer");
 const { diskStorage } = multer;
 const { join, dirname } = require("path");
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
-
 const storage = diskStorage({
   destination: (req, file, callback) => {
     const __dirname = dirname(__filename);
     callback(null, join(__dirname, "../public/images"));
   },
   filename: (req, file, callback) => {
-    // Générer un nom de fichier unique sans le timestamp et l'extension supplémentaire
-    const extension = MIME_TYPES[file.mimetype];
-    const nomFichierSansExtension = file.originalname.split(" ").join("_").split(".").slice(0, -1).join(".");
-    callback(null, nomFichierSansExtension + "_" + Date.now() + "." + extension);
+    // Utiliser le nom de fichier d'origine sans aucune modification
+    callback(null, file.originalname);
   },
 });
 
