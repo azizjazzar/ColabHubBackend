dbe5b98ff5ffeff92b39c388fd83d3ff750b6bdc
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-02-22 01:46:52 +0100

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const multerUpload = require("../middleware/multer-config.js");
+const multerUpload = require("../middleware/multer.js");
 const {
   register,
   users,
