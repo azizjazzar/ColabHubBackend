435d2cd51130000a33bb9e6febb8e912fceeb48c
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-03-12 21:32:49 +0100

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,17 +1,14 @@
-
 // Import dependencies
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser'); // Import body-parser
 const paymentController = require('../controllers/paymentController');
 
-// Use body-parser middleware to parse request bodies
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
-
 // Route to create a PaymentIntent
 router.post('/create-checkout-session', paymentController.createCheckoutSession);
-router.post('/webhook', paymentController.stripeWebhook);
+
+// Route to handle Stripe webhook
+router.post('/webhook', bodyParser.raw({ type: 'application/json' }), paymentController.stripeWebhook);
 
 // Export the router
 module.exports = router;
