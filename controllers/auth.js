9b071d1551ea1193d69e8b422247a2a8eba13cd2
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-03-27 21:30:23 +0100

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -172,6 +172,46 @@ exports.geminiAnalyse = async (req, res, next) => {
   }
 };
 
+exports.geminiAnalyseWithText = async (req, res, next) => {
+  const { transcribedText } = req.body;
+  const { text } = req.body;
+
+
+  try {
+    const googleGeminiURL = 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent';
+
+    const requestBody = {
+      contents: [
+        {
+          role: 'user',
+          parts: [
+            {
+              text:  `${text}  ${transcribedText}`
+            }
+          ]
+        }
+      ]
+    };
+
+    const response = await axios.post(googleGeminiURL, requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-goog-api-key': process.env.GEMINIKEY
+      }
+    });
+
+    // Récupérer le texte généré à partir de la réponse
+    const generatedText = response.data.candidates[0].content.parts[0].text;
+
+    // Envoyer le texte généré en réponse
+    res.json({ answer: generatedText });
+  } catch (error) {
+    // Gérer les erreurs ici
+    console.error('Erreur lors de la requête à Google Gemini:', error);
+    res.status(500).json({ message: 'Une erreur s\'est produite lors de la requête à Google Gemini' });
+  }
+};
+
 
 
 exports.chatgptAnalyse = async (req, res, next) => {
