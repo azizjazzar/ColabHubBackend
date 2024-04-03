ae39d52c028dfefb733306f4e71235a0f440cfa9
mohamedaziztemimi <80290944+mohamedaziztemimi@users.noreply.github.com>
2024-04-03 02:19:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -93,6 +93,6 @@ io.on("connection", (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
