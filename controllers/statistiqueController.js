6d573c01b0cbb08193cdcf61cc363030c9f74e60
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-04-03 06:22:58 +0100

    Update statistiqueController.js

--- controllers/statistiqueController.js
+++ controllers/statistiqueController.js
@@ -2,43 +2,20 @@ const Meeting = require('../models/Statistique');
 const User = require('../models/User'); 
 
 // Ajouter une statistique
-
-exports.getMeetingByTokenAndChannel = async (req, res) => {
-  try {
-    const { channel, token } = req.body; 
-    const results = await Meeting.findOne({ channel, token });
-    res.json(results);
-  } catch (err) {
-    res.status(500).send({ message: "Erreur lors de la recherche des réunions", error: err.message });
-  }
-};
-
-exports.getCountOfStatistiques = async (req, res) => {
-  try {
-    // Compter le nombre total de documents dans la collection
-    const count = await Meeting.countDocuments();
-
-    // Renvoyer le nombre total en tant que réponse JSON
-    res.status(200).json({ count });
-  } catch (error) {
-    console.error('Erreur lors de la récupération du nombre de statistiques :', error);
-    res.status(500).json({ error: 'Erreur lors de la récupération du nombre de statistiques' });
-  }
-};
-
 exports.addStatistique = async (req, res) => {
   try {
-    const { clientA, clientB, dateEnrg, token, clientAID, clientBID, channel, responseClientA, responseClientB } = req.body;
+    const { clientA, clientB, dateEnrg, token, clientAID, clientBID, channel, responseClientA, responseClientB, status } = req.body;
     const nouvelleStatistique = new Meeting({
-      clientA: clientA,
-      clientB: clientB,
-      clientAID: clientAID,
-      clientBID: clientBID,
-      dateEnrg: dateEnrg,
-      channel: channel,
-      token: token,
-      responseClientA: responseClientA,
-      responseClientB: responseClientB
+      clientA,
+      clientB,
+      clientAID,
+      clientBID,
+      dateEnrg,
+      token,
+      channel,
+      responseClientA,
+      responseClientB,
+      status // Ajout du champ status
     });
     const statistiqueEnregistree = await nouvelleStatistique.save();
     console.log('Statistique ajoutée avec succès :', statistiqueEnregistree);
@@ -49,15 +26,31 @@ exports.addStatistique = async (req, res) => {
   }
 };
 
-exports.isClientAEmpty = async (req, res) => {
-  const { token, channel } = req.body;
+// Mettre à jour une statistique par ID
+exports.updateStatistiqueById = async (req, res) => {
+  const { id } = req.params;
+  const { clientA, clientB, dateEnrg, token, clientAID, clientBID, channel, responseClientA, responseClientB, status } = req.body;
   try {
-    const statistique = await Meeting.findOne({ token, channel });
-    const isClientAEmpty = !statistique || !statistique.clientA;
-    res.json(isClientAEmpty);
+    const statistique = await Meeting.findByIdAndUpdate(id, {
+      clientA,
+      clientB,
+      clientAID,
+      clientBID,
+      dateEnrg,
+      token,
+      channel,
+      responseClientA,
+      responseClientB,
+      status // Ajout du champ status
+    }, { new: true });
+    if (!statistique) {
+      return res.status(404).json({ error: 'Statistique non trouvée' });
+    }
+    console.log('Statistique mise à jour avec succès :', statistique);
+    res.status(200).json(statistique);
   } catch (error) {
-    console.error('Error checking if clientA is empty:', error);
-    res.status(500).json({ error: 'Error checking if clientA is empty' });
+    console.error('Erreur lors de la mise à jour de la statistique :', error);
+    res.status(500).json({ error: 'Erreur lors de la mise à jour de la statistique' });
   }
 };
 
@@ -87,33 +80,6 @@ exports.getStatistiqueById = async (req, res) => {
   }
 };
 
-// Mettre à jour une statistique par ID
-exports.updateStatistiqueById = async (req, res) => {
-  const { id } = req.params;
-  const { clientA, clientB, dateEnrg, token, clientAID, clientBID, channel, responseClientA, responseClientB } = req.body;
-  try {
-    const statistique = await Meeting.findByIdAndUpdate(id, {
-      clientA: clientA,
-      clientB: clientB,
-      clientAID: clientAID,
-      clientBID: clientBID,
-      dateEnrg: dateEnrg,
-      channel: channel,
-      token: token,
-      responseClientA: responseClientA,
-      responseClientB: responseClientB
-    }, { new: true });
-    if (!statistique) {
-      return res.status(404).json({ error: 'Statistique non trouvée' });
-    }
-    console.log('Statistique mise à jour avec succès :', statistique);
-    res.status(200).json(statistique);
-  } catch (error) {
-    console.error('Erreur lors de la mise à jour de la statistique :', error);
-    res.status(500).json({ error: 'Erreur lors de la mise à jour de la statistique' });
-  }
-};
-
 // Supprimer une statistique par ID
 exports.deleteStatistiqueById = async (req, res) => {
   const { id } = req.params;
@@ -129,6 +95,7 @@ exports.deleteStatistiqueById = async (req, res) => {
     res.status(500).json({ error: 'Erreur lors de la suppression de la statistique' });
   }
 };
+
 exports.getall = async (req, res) => {
   try {
     // Retrieve all meetings
@@ -156,5 +123,40 @@ exports.getall = async (req, res) => {
   } catch (error) {
     console.error('Erreur lors de la récupération des statistiques :', error);
     res.status(500).json({ error: 'Erreur lors de la récupération des statistiques' });
+  }
+};
+
+exports.getMeetingByTokenAndChannel = async (req, res) => {
+  try {
+    const { channel, token } = req.body; 
+    const results = await Meeting.findOne({ channel, token });
+    res.json(results);
+  } catch (err) {
+    res.status(500).send({ message: "Erreur lors de la recherche des réunions", error: err.message });
+  }
+};
+
+exports.getCountOfStatistiques = async (req, res) => {
+  try {
+    // Compter le nombre total de documents dans la collection
+    const count = await Meeting.countDocuments();
+
+    // Renvoyer le nombre total en tant que réponse JSON
+    res.status(200).json({ count });
+  } catch (error) {
+    console.error('Erreur lors de la récupération du nombre de statistiques :', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération du nombre de statistiques' });
+  }
+};
+
+exports.isClientAEmpty = async (req, res) => {
+  const { token, channel } = req.body;
+  try {
+    const statistique = await Meeting.findOne({ token, channel });
+    const isClientAEmpty = !statistique || !statistique.clientA;
+    res.json(isClientAEmpty);
+  } catch (error) {
+    console.error('Error checking if clientA is empty:', error);
+    res.status(500).json({ error: 'Error checking if clientA is empty' });
   }
 };
