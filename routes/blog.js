221251f1f49b593aa7d319c9fd428dce4e46804f
idrisselbessi <93952066+idrisselbessi@users.noreply.github.com>
2024-04-16 19:42:44 +0100

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/BlogController');
 
+
 // Basic CRUD operations for blogs
 router.post('/addBlog', blogController.createBlog);
 router.get('/blogs', blogController.getAllBlogs);
@@ -14,5 +15,11 @@ router.post('/:id/comments', blogController.addComment);
 router.get('/:id/comments', blogController.getCommentsByBlogId);
 // User-specific blogs route
 router.get('/user/:userId/blogs', blogController.getBlogsByUser);
+// Like a comment
+router.post('/:blogId/comments/:commentId/like', blogController.likeComment);
+// Dislike a comment
+router.post('/:blogId/comments/:commentId/dislike', blogController.dislikeComment);
+
+
 
 module.exports = router;
