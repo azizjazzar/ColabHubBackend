42af1566ad2b5a6f500d792552c9620775221d9b
idrisselbessi <93952066+idrisselbessi@users.noreply.github.com>
2024-04-16 19:42:04 +0100

    Update blog.js

--- models/blog.js
+++ models/blog.js
@@ -14,6 +14,24 @@ const commentSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    likes: [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true,
+            },
+        }
+    ],
+    dislikes: [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true,
+            },
+        }
+    ],
 });
 
 const blogSchema = new mongoose.Schema({
@@ -34,6 +52,13 @@ const blogSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    category: {  
+        type: String,
+        required: true, // La catégorie est maintenant requise
+        enum: ['Web', 'Mobile', 'Design', 'Blockchain', 'Cybersecurity', 'Data Science', 'Cloud', 'Other'], // Définir les catégories acceptées
+        default: 'Other', // Catégorie par défaut
+    },
+    
     date: {
         type: Date,
         default: Date.now,
