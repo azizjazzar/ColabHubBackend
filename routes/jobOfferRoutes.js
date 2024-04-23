3e848b5d75315b945d046daa9d28d8e92fc16a94
mohamedaziztemimi <80290944+mohamedaziztemimi@users.noreply.github.com>
2024-04-23 23:28:41 +0200

    Update jobOfferRoutes.js

--- routes/jobOfferRoutes.js
+++ routes/jobOfferRoutes.js
@@ -9,9 +9,9 @@ router.post('/add', JobController.createJobOffer);
 router.get('/get', JobController.getAllJobOffers);
 
 router.get('/get/:jobId', JobController.getJobOfferById);
-
-router.put('/update/:jobId', JobController.updateJobOffer);
 router.get('/getFreelancersByJob/:jobId', JobController.getAllFreelancerByJob);
+
+router.get('/freelancers/:freelancerId', JobController.getJobsForFreelancer);
 router.delete('/delete/:jobId', JobController.deleteJobOffer);
 
 module.exports = router;
