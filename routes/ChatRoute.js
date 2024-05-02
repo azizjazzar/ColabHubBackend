9ebe3b321391cb1da34732970c251529b365e971
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-05-02 20:07:26 +0100

    Update ChatRoute.js

--- routes/ChatRoute.js
+++ routes/ChatRoute.js
@@ -11,7 +11,7 @@ const {
 } = require("../controllers/chatControllers");
 const { verifyTokenMiddleware } = require("../middleware/auth");
 
-router.route("/searchUser").get(verifyTokenMiddleware, allUsers);
+router.route("/searchUser").get( allUsers);
 router.route("/acceschat").post(verifyTokenMiddleware, accessChat);
 router.route("/fetchchat").get(verifyTokenMiddleware, fetchChats);
 router.route("/addgroupe").post(verifyTokenMiddleware, createGroupChat);
