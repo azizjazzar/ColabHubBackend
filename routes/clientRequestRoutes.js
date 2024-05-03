469aa440601b2e80b89eec4f4ce5954aa5c0dd8f
raniahachem <80328069+raniahachem@users.noreply.github.com>
2024-05-03 09:47:35 +0100

    Update clientRequestRoutes.js

--- routes/clientRequestRoutes.js
+++ routes/clientRequestRoutes.js
@@ -5,5 +5,6 @@ const clientRequestController = require('../controllers/clientRequestController'
 // Endpoint pour enregistrer une demande client
 router.post('/saveClientRequest', clientRequestController.saveClientRequest);
 router.get('/total/:serviceId', clientRequestController.getTotalRequestsByServiceId);
+router.get('/:serviceId', clientRequestController.getClientRequestsByServiceId);
 
 module.exports = router;
