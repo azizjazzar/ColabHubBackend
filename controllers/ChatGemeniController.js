43fd0321da0e0b9bf8e424bb7abcba675c916a7a
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-05-29 05:20:13 +0100

    updates

--- controllers/ChatGemeniController.js
+++ controllers/ChatGemeniController.js
@@ -2,7 +2,7 @@ const ChatGemeni = require('../models/ChatGemeni');
 
 exports.saveResponse = async (req, res) => {
     try {
-        const { role, message,userId } = req.body;
+        const { role, message, userId } = req.body;
         const response = await ChatGemeni.create({
             role: role,
             message: message,
@@ -15,3 +15,54 @@ exports.saveResponse = async (req, res) => {
         res.status(500).json({ message: "Une erreur s'est produite lors de l'enregistrement de la réponse." });
     }
 };
+
+exports.getAllResponses = async (req, res) => {
+    try {
+        const allResponses = await ChatGemeni.find();
+        res.status(200).json(allResponses);
+    } catch (error) {
+        console.error("Erreur lors de la récupération de toutes les réponses :", error);
+        res.status(500).json({ message: "Une erreur s'est produite lors de la récupération de toutes les réponses." });
+    }
+};
+
+exports.geminiWithText = async (req, res) => {
+    const { text } = req.body;
+  
+    try {
+      const googleGeminiURL =
+        "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent";
+  
+      const requestBody = {
+        contents: [
+          {
+            role: "user",
+            parts: [
+              {
+                text: `${text}`,
+              },
+            ],
+          },
+        ],
+      };
+  
+      const response = await axios.post(googleGeminiURL, requestBody, {
+        headers: {
+          "Content-Type": "application/json",
+          "x-goog-api-key": process.env.GEMINIKEY,
+        },
+      });
+  
+      // Récupérer le texte généré à partir de la réponse
+      const generatedText = response.data.candidates[0].content.parts[0].text;
+  
+      // Envoyer le texte généré en réponse
+      res.json({ answer: generatedText });
+    } catch (error) {
+      // Gérer les erreurs ici
+      console.error("Erreur lors de la requête à Google Gemini:", error);
+      res.status(500).json({
+        message: "Une erreur s'est produite lors de la requête à Google Gemini",
+      });
+    }
+  };
--- routes/ChatgemeniRoute.js
+++ routes/ChatgemeniRoute.js
@@ -5,6 +5,8 @@ const ChatGemeniController = require('../controllers/ChatGemeniController');
 
 // Basic CRUD operations for blogs
 router.post('/add', ChatGemeniController.saveResponse);
+router.get('/getall', ChatGemeniController.getAllResponses);
+router.post('/chatwithgemeni', ChatGemeniController.geminiWithText);
 
 
 module.exports = router;
