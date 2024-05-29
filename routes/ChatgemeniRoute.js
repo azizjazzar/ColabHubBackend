88dc64bab7d8ae428fe522d0086c2ec04f266025
azizjazzar <80291781+azizjazzar@users.noreply.github.com>
2024-05-29 05:42:37 +0100

    updates

--- routes/ChatgemeniRoute.js
+++ routes/ChatgemeniRoute.js
@@ -6,7 +6,7 @@ const ChatGemeniController = require('../controllers/ChatGemeniController');
 // Basic CRUD operations for blogs
 router.post('/add', ChatGemeniController.saveResponse);
 router.get('/getall', ChatGemeniController.getAllResponses);
-router.get('/getallbyid', ChatGemeniController.getAllResponsesById);
+router.get('/getallbyid/:id', ChatGemeniController.getAllResponsesById);
 router.post('/chatwithgemeni', ChatGemeniController.geminiWithText);
 
 module.exports = router;
